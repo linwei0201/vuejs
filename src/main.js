8800b5cea74fd1463efcdf0b652d3c08d3cea026
Lin Wei <linwei0201@126.com>
2018-03-05 15:20:32 +0800

    aa

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { Button, Select, Option, Row, Col, Table, TableColumn } from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/common.scss'
 
 Vue.config.productionTip = false
 
